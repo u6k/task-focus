4e40cca283d674e6a9fb847305c705c73ce0fc92
u6k <u6k.apps@gmail.com>
2017-08-14 16:19:59 +0900

    条件判断、状態変更、処理呼び出しなどでTRACEログを出力するようにしました。

--- src/main/java/me/u6k/task_focus/controller/TaskUIController.java
+++ src/main/java/me/u6k/task_focus/controller/TaskUIController.java
@@ -4,6 +4,8 @@ package me.u6k.task_focus.controller;
 import java.util.Date;
 
 import me.u6k.task_focus.service.TaskService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,29 +18,44 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class TaskUIController {
 
+    private static final Logger L = LoggerFactory.getLogger(TaskUIController.class);
+
     @Autowired
     private TaskService taskService;
 
     @RequestMapping(value = "/ui/tasks/add", method = RequestMethod.GET)
     public String addInit(@ModelAttribute("form") TaskVO form, Model model) {
-        form.setDate(new Date());
+        L.debug("#addInit: form={}, model={}", form, model);
 
+        form.setDate(new Date());
+        L.debug("setup form: form={}", form);
+
+        L.debug("return");
         return "tasks-add";
     }
 
     @RequestMapping(value = "/ui/tasks/add", method = RequestMethod.POST)
     public String add(@Validated @ModelAttribute("form") TaskVO form, BindingResult result, Model model) {
+        L.debug("add: form={}, result={}, model={}", form, result, model);
+
+        L.debug("validate: hasErrors={}", result.hasErrors());
         if (result.hasErrors()) {
+            L.debug("return");
             return "tasks-add";
         }
 
         try {
             this.taskService.create(form.getDate(), form.getName(), form.getEstimatedTime(), form.getEstimatedStartTime());
+            L.debug("taskService.create: success");
         } catch (IllegalArgumentException e) {
             model.addAttribute("error", e.getMessage());
+            L.debug("setup model: model={}", model);
+
+            L.debug("return");
             return "tasks-add";
         }
 
+        L.debug("return");
         return "redirect:/ui/tasks";
     }
 
--- src/main/java/me/u6k/task_focus/service/TaskService.java
+++ src/main/java/me/u6k/task_focus/service/TaskService.java
@@ -1,7 +1,6 @@
 
 package me.u6k.task_focus.service;
 
-import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
@@ -10,8 +9,11 @@ import javax.transaction.Transactional;
 
 import me.u6k.task_focus.model.Task;
 import me.u6k.task_focus.model.TaskRepository;
+import me.u6k.task_focus.util.DateUtil;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.time.DateUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,24 +21,37 @@ import org.springframework.stereotype.Service;
 @Transactional
 public class TaskService {
 
+    private static final Logger L = LoggerFactory.getLogger(TaskService.class);
+
     @Autowired
     private TaskRepository taskRepo;
 
     public UUID create(Date date, String name, int estimatedTime, Date estimatedStartTime) {
+        L.trace("#create: date={}, name={}, estimatedTime={}, estimatedStartTime={}", date, name, estimatedTime, estimatedStartTime);
+
         /*
          * 入力チェック
          */
+        L.trace("validate: date={}", date);
         if (date == null) {
             throw new IllegalArgumentException("date is null.");
         }
+
+        L.trace("validate: name={}", name);
         if (StringUtils.isBlank(name)) {
             throw new IllegalArgumentException("name is blank.");
         }
+
+        L.trace("validate: estimatedTime={}", estimatedTime);
         if (estimatedTime < 0) {
             throw new IllegalArgumentException("estimatedTime < 0. estimatedTime=" + estimatedTime);
         }
+
+        L.trace("validate: estimatedStartTime={}", estimatedStartTime);
         if (estimatedStartTime != null) {
-            if (!DateUtils.isSameDay(date, estimatedStartTime)) {
+            boolean isSameDay = DateUtils.isSameDay(date, estimatedStartTime);
+            L.trace("validate: isSameDay={}", isSameDay);
+            if (!isSameDay) {
                 throw new IllegalArgumentException("date and estimatedStartTime are different day. date=" + date + ", estimatedStartTime=" + estimatedStartTime);
             }
         }
@@ -46,7 +61,7 @@ public class TaskService {
          */
         Task task = new Task();
         task.setId(UUID.randomUUID());
-        task.setDate(date);
+        task.setDate(DateUtil.datetimeToDate(date));
         // TODO 正しい作業順を設定する。
         task.setOrderOfDate(0);
         task.setName(name.trim());
@@ -54,16 +69,22 @@ public class TaskService {
         task.setEstimatedStartTime(estimatedStartTime);
         task.setStartTime(null);
         task.setEndTime(null);
+        L.trace("new Task: task={}", task);
 
         this.taskRepo.save(task);
+        L.trace("taskRepo.save: success");
 
+        L.trace("return: task.getId={}", task.getId());
         return task.getId();
     }
 
     public List<Task> findByDate(Date date) {
+        L.trace("#findByDate: date={}", date);
+
         /*
          * 入力チェック
          */
+        L.trace("validate: date={}", date);
         if (date == null) {
             throw new IllegalArgumentException("date is null.");
         }
@@ -72,9 +93,12 @@ public class TaskService {
          * タスクを検索
          */
         date = DateUtil.datetimeToDate(date);
+        L.trace("datetimeToDate: date={}", date);
 
         List<Task> taskList = this.taskRepo.findByDate(date);
+        L.trace("findByDate: taskList.size={}", taskList.size());
 
+        L.trace("return: taskList.size={}", taskList.size());
         return taskList;
     }
 
