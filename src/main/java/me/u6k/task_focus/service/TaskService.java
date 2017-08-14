1aa37393fb09cc05da745c72f4cab8bdd3da72e2
u6k <u6k.apps@gmail.com>
2017-08-14 19:59:02 +0900

    "javax.transaction.Transactional"を使用するとjavadocタスクが失敗するので、"org.springframework.transaction.annotation.Transactional"に変更しました。

--- src/main/java/me/u6k/task_focus/service/TaskService.java
+++ src/main/java/me/u6k/task_focus/service/TaskService.java
@@ -5,8 +5,6 @@ import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
-import javax.transaction.Transactional;
-
 import me.u6k.task_focus.model.Task;
 import me.u6k.task_focus.model.TaskRepository;
 import me.u6k.task_focus.util.DateUtil;
@@ -16,6 +14,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
