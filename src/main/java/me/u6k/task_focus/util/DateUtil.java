bdcf231baf0a33c88e85eb28cbc7e6c7c1c67b23
u6k <u6k.apps@gmail.com>
2017-08-15 18:53:07 +0900

    日付操作に処理を追加し、Optionalに対応しました。

--- src/main/java/me/u6k/task_focus/util/DateUtil.java
+++ src/main/java/me/u6k/task_focus/util/DateUtil.java
@@ -2,8 +2,9 @@
 package me.u6k.task_focus.util;
 
 import java.text.ParseException;
-import java.util.Calendar;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Optional;
 
 import org.apache.commons.lang3.time.DateUtils;
 
@@ -11,34 +12,47 @@ public final class DateUtil {
 
     private static final String FORMAT_FULL_DATETIME = "yyyy-MM-dd HH:mm:ss.SSS";
 
+    private static final String FORMAT_DATE = "yyyy-MM-dd";
+
+    private static final String FORMAT_HOUR_MINUTE = "HH:mm";
+
     private DateUtil() {
     }
 
-    public static Date datetimeToDate(Date date) {
-        Date newDate = DateUtils.setHours(date, 0);
-        newDate = DateUtils.setMinutes(newDate, 0);
-        newDate = DateUtils.setSeconds(newDate, 0);
-        newDate = DateUtils.setMilliseconds(newDate, 0);
-
-        return newDate;
+    public static Optional<Date> parseFullDatetime(Optional<String> str) throws ParseException {
+        return parse(str, FORMAT_FULL_DATETIME);
     }
 
-    public static Date hourMinutesToDatetime(Date date) {
-        Date newDate = new Date();
-        Calendar calendar = DateUtils.toCalendar(date);
-
-        newDate = DateUtils.setHours(newDate, calendar.get(Calendar.HOUR_OF_DAY));
-        newDate = DateUtils.setMinutes(newDate, calendar.get(Calendar.MINUTE));
-        newDate = DateUtils.setSeconds(newDate, 0);
-        newDate = DateUtils.setMilliseconds(newDate, 0);
-
-        return newDate;
+    public static Optional<Date> parseDate(Optional<String> str) throws ParseException {
+        return parse(str, FORMAT_DATE);
     }
 
-    public static Date parseFullDatetime(String str) throws ParseException {
-        Date date = DateUtils.parseDate(str, FORMAT_FULL_DATETIME);
+    public static Optional<Date> parseHourMinute(Optional<String> str) throws ParseException {
+        return parse(str, FORMAT_HOUR_MINUTE);
+    }
+
+    public static Optional<String> formatDate(Optional<Date> date) {
+        return format(date, FORMAT_DATE);
+    }
+
+    public static Optional<String> formatHourMinute(Optional<Date> date) {
+        return format(date, FORMAT_HOUR_MINUTE);
+    }
+
+    private static Optional<Date> parse(Optional<String> str, String format) throws ParseException {
+        if (!str.isPresent()) {
+            return Optional.empty();
+        }
+
+        Optional<Date> date = Optional.of(DateUtils.parseDate(str.get(), format));
 
         return date;
+    }
+
+    private static Optional<String> format(Optional<Date> date, String format) {
+        Optional<String> str = date.map(x -> new SimpleDateFormat(format).format(x));
+
+        return str;
     }
 
 }
