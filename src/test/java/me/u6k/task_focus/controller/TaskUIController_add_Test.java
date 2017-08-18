0fcf9a06330d57f8c53b30d2fcf5b786619636f8
u6k <u6k.apps@gmail.com>
2017-08-18 15:02:40 +0900

    コントローラー・テストにRANDOM_PORTを指定するようにしました。

--- src/test/java/me/u6k/task_focus/controller/TaskUIController_add_Test.java
+++ src/test/java/me/u6k/task_focus/controller/TaskUIController_add_Test.java
@@ -16,12 +16,13 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 public class TaskUIController_add_Test {
 
